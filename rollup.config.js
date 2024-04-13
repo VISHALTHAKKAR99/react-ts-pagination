8cbe09067d0d817dd6158f800af1880244c08598
VISHALTHAKKAR99 <vishal.thakkar@brainvire.com>
2024-04-13 11:47:32 +0530

    first version commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,53 @@
+// import { defineConfig } from "rollup";
+// import typescript from "@rollup/plugin-typescript";
+// import postcss from 'rollup-plugin-postcss';
+
+// export default defineConfig({
+//     input: 'src/index.tsx',
+//     output: {
+//         file: 'dist/bundle.js',
+//         format: 'es',
+//         name: 'react-ts-paginations'
+//     },
+
+//     external: ["react/jsx-runtime", "react-dom/client", "web-vitals"],
+
+//     plugins: [typescript({ tsconfig: "tsconfig.json" },
+
+//         postcss({
+//             extract: true,
+//             // modules: true,
+//             minimize: true,
+//             plugins:[
+//                 require('tailwindcss'), 
+//                 require('postcss-import'),
+                
+//             ]
+//         })
+//     )
+//     ]
+// })
+
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import postcss from 'rollup-plugin-postcss';
-
-export default defineConfig({
-    input:'src/App.tsx',
-    output:{
-        dir:'dist',
-        format:'es',
-        name:'react-ts-paginations'
+export default {
+    input: 'src/index.tsx',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'es',
+        name: 'react-ts-paginations'
     },
-    external:["react","react-dom"],
-    plugins:[typescript({tsconfig:"tsconfig.json"},
-
-    postcss({
-        extract: true, // Extract CSS to a separate file
-        modules: true, // Enable CSS modules
-        minimize: true // Minimize CSS
-      })
-)
-]
-})
+    external: ["react", "react/jsx-runtime", "react-dom/client", "web-vitals"],
+    plugins: [
+        typescript({ tsconfig: "tsconfig.json" }),
+        postcss({
+            extract: true,
+            minimize: true,
+            plugins: [
+                require('tailwindcss'),
+                require('postcss-import')
+            ]
+        })
+    ]
+};
